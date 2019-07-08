b84f55795e9310215be3cc8160e97502ad42179e
devannea <devannenotes@gmail.com>
2019-07-07 20:17:24 -0500

    Added fizzbuzz cpp

---
+++ FizzBuzz/FizzBuzz.cpp
@@ -0,0 +1,26 @@
+// FizzBuzz.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 30; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz\t");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz\t");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz\t");
+		}
+		else { printf("%d\t", i); }
+	}
+	return 0;
+}
