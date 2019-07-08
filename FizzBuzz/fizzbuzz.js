94126dd170726c5bc3ab0d84b2731a0e2bcd6285
devannea <devannenotes@gmail.com>
2019-07-07 19:23:49 -0500

    Added fizzbuzz js

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -2,15 +2,15 @@ for ( i = 0; i <= 30; i++ )
 {
     if ( i % 15 == 0 ) // if i is divisible by both 3 and 5, which would be 15
     {
-        console.log('FizzBuzz');
+        console.log(i + 'FizzBuzz');
     }
     else if ( i % 3 == 0 ) // if i is divisible by 3
     {
-        console.log('Fizz');
+        console.log(i + 'Fizz');
     }
     else if ( i % 5 == 0 ) // if i is divisible by 5
     {
-        console.log('Buzz');
+        console.log(i + 'Buzz');
     }
-    else { console.log(' '); }
+    else { console.log(i); }
 }
